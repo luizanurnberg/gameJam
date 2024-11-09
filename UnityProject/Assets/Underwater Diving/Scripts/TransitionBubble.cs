ba2b8ce2645936d1ac779b94f6ac8f2c4345d577
maariaceciliaholler <mariaceciliaholler@gmail.com>
2024-11-09 10:13:25 -0300

    feat(scene): add effect transition

---
+++ UnityProject/Assets/Underwater Diving/Scripts/TransitionBubble.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionBubble : MonoBehaviour
+{
+    public string sceneName;
+    public GameObject bubbleEffectPrefab;
+    public AudioClip bubbleSound;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+
+        StartCoroutine(TransitionWithDelay());
+    }
+
+    private IEnumerator TransitionWithDelay()
+    {
+        GameObject bubbleEffect = Instantiate(bubbleEffectPrefab, transform.position, Quaternion.identity);
+
+        if (bubbleSound != null)
+        {
+            audioSource.clip = bubbleSound;
+            audioSource.Play();
+        }
+
+        yield return new WaitForSeconds(5);
+
+        Destroy(bubbleEffect);
+        SceneManager.LoadScene(sceneName);
+    }
+}
