cb495d3e968e54c3ab332a0d95834d65ab3bc99b
Rafael Dalmarco <111667173+rdalmarco@users.noreply.github.com>
2024-11-08 22:38:50 -0300

    Add Pontuation HurtPlayer
    Add Pontuation HurtPlayer

--- UnityProject/Assets/Underwater Diving/Scripts/HurtPlayer.cs
+++ UnityProject/Assets/Underwater Diving/Scripts/HurtPlayer.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HurtPlayer : MonoBehaviour {
+public class HurtPlayer : MonoBehaviour
+{
 
-	private PlayerController thePlayer;
+    private PlayerController thePlayer;
+    public Text messageText;
+    private static int pontuacao;
 
-	// Use this for initialization
-	void Start () {
-		thePlayer = FindObjectOfType<PlayerController> ();	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
 
-	void OnTriggerEnter2D(Collider2D other){
-		if(other.tag == "Player"){
-			thePlayer.hurt ();	 
-		}
+        // Procura o objeto com a tag e pega o componente Text
+        GameObject messageObject = GameObject.FindWithTag("MessageText");
+        if (messageObject != null)
+        {
+            messageText = messageObject.GetComponent<Text>();
+        }
+    }
 
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            thePlayer.hurt();
+            pontuacao += 1;
+            messageText.text = ("Pontuação: " + pontuacao.ToString());
+        }
+
+    }
 }
