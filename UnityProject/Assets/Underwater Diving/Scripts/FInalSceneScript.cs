1912fba33f3bc337dae152b914189d9076d07ac1
tobiasfkk <78160146+tobiasfkk@users.noreply.github.com>
2024-11-09 11:47:15 -0300

    criado tela final

---
+++ UnityProject/Assets/Underwater Diving/Scripts/FInalSceneScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Adicionar para usar UI
+
+public class FinalSceneScript : MonoBehaviour
+{
+    public int pontuacao;
+
+    // Referências aos objetos de texto
+    public Text finalSceneTitulo;
+    public Text finalScenePontuacao;
+
+    private void Start()
+    {
+
+
+        //na tela que chamar esse script, chamar assim:
+        // PlayerPrefs.SetInt("Pontuacao", pontuacao);
+        // SceneManager.LoadScene("FinalScene");
+
+
+        pontuacao = PlayerPrefs.GetInt("Pontuacao", 0);
+
+        if (pontuacao > 10)
+        {
+            finalSceneTitulo.text = "VOCE GANHOU!";
+        }
+        else
+        {
+            finalSceneTitulo.text = "VOCE PERDEU!";
+        }
+
+        finalScenePontuacao.text = "Pontuacao: " + pontuacao;
+    }
+}
