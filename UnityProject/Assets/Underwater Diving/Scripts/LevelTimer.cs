853113a953929c3dff84b7900324a0329631dd34
Matheus Buschermoehle <73143728+matheusbus@users.noreply.github.com>
2024-11-09 12:12:38 -0300

    feat(timer): Timer added to all levels

---
+++ UnityProject/Assets/Underwater Diving/Scripts/LevelTimer.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour
+{
+
+    public float levelTime = 30f;
+    private float timeRemaining;
+    public Text timerText;
+    public GameObject gameOverScreen;
+
+
+    void Start()
+    {
+        timeRemaining = levelTime;
+    }
+
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+
+        if (timerText != null)
+        {
+            timerText.text = "Tempo: " + Mathf.Ceil(timeRemaining).ToString();
+        }
+
+        if (timeRemaining <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("O tempo acabou!");
+
+        /*
+        if (gameOverScreen != null)
+        {
+            gameOverScreen.SetActive(true);
+        }
+        */
+
+
+        PlayerPrefs.SetInt("Pontuacao", 10); // valor qualquer por enquanto
+        SceneManager.LoadScene("FinalScene");
+
+        // Opcional: pause o jogo
+        Time.timeScale = 0f;
+    }
+}
