5f433c9f63b4a2c666787fb667990db7edba095d
maariaceciliaholler <mariaceciliaholler@gmail.com>
2024-11-09 01:49:06 -0300

    feat(scene): transition scenes

---
+++ UnityProject/Assets/Underwater Diving/Scripts/Transition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    // Nome da cena para a qual o player será transportado
+    public string sceneName;
+
+    // Detecta a colisão com o player
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // Verifica se o objeto é o player
+        {
+            SceneManager.LoadScene(sceneName); // Carrega a nova cena
+        }
+    }
+}
